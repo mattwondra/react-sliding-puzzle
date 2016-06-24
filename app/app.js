76e60c141cd787002457e9cf121e42a802d2f04f
Matt Wondra <mattwondra@gmail.com>
2016-06-24 00:05:49 -0500

    Bootstrap in place

---
+++ app/app.js
@@ -0,0 +1,4 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(<div>Holla, world</div>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let config = {
+  entry: {
+    app: './app/app.js'
+  },
+  output: {
+    path: './build',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;
